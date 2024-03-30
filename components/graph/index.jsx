d404ec147d2ac64d7a02ca5118f9f2328835bfc6
Muhammad Sameer Naveed <68469246+Sameer-dot@users.noreply.github.com>
2024-03-30 20:53:27 +0500

    Career's Path Page Created

---
+++ components/graph/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Graph = () => {
+  return (
+    <div className="w-[100%] h-[360px] rounded bg-gray-500 mt-6 flex justify-center items-center text-black">
+      Graph
+    </div>
+  );
+};
+
+export default Graph;
