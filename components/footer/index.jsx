a4742185339b8907b2d756799fe6358a5de46e88
NivyanB <bnivyan09@gmail.com>
2024-03-30 21:22:40 +0500

    footer added

---
+++ components/footer/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="text-center py-4 bg-nemo-dark">
+      Powered by nemo @‌2024
+    </div>
+  );
+};
+
+export default Footer;
--- pages/layout.js
+++ pages/layout.js
@@ -1,10 +1,12 @@
 import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 export default function Layout({ children }) {
   return (
     <>
       <Navbar />
       {children}
+      <Footer/>
     </>
   );
 }
