63645faecd3b41fc1afc30d6f21bd1a1c7fe77b3
NivyanB <bnivyan09@gmail.com>
2024-04-02 21:19:35 +0500

    footer removed

--- pages/layout.js
+++ pages/layout.js
@@ -1,12 +1,10 @@
 import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
 
 export default function Layout({ children }) {
   return (
     <main className="flex flex-col min-h-screen">
       <Navbar />
       {children}
-      <Footer />
     </main>
   );
 }
