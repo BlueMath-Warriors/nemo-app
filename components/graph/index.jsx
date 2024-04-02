5d0f442d4a80bd11d9d124589274077c8b5acf77
Muhammad Sameer Naveed <68469246+Sameer-dot@users.noreply.github.com>
2024-04-02 23:48:53 +0500

    Careers Path Page Changes added

--- components/graph/index.jsx
+++ components/graph/index.jsx
@@ -5,34 +5,44 @@ const Graph = ({ data1, label1, data2, label2 }) => {
   const options = {
     plugins: {
       legend: {
+        position: "bottom",
         labels: {
           color: "white",
           font: {
             size: 12,
           },
+          boxWidth: 12,
         },
+        marginTop: 60,
       },
     },
+    layout: {
+      padding: {
+        top: 20,
+      },
+    },
+    responsive: true,
   };
 
   return (
     <>
       <div className="mt-4 flex justify-center items-center">
-        <p className="w-[33%] h-[360px] rounded mt-4 p-6 text-white mr-2">
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
-          et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
-          felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
-          consequat massa quis enim. Donec pede justo, fringilla vel, aliquet
-          nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a,
-          venenatis vitae, justo. Nullam dictum.
+        <p className="w-[33%] h-[360px] rounded pt-1 p-6 text-white mr-2">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error
+          repellendus explicabo assumenda illo neque qui nam voluptatibus quidem
+          ab veniam tenetur eaque, ad excepturi eos est asperiores nemo dolor
+          possimus sed sint! Aliquam qui dignissimos cupiditate quaerat fugit
+          quis dolorem facere quidem eos cumque, alias odio maxime, harum eum
+          vero quos dolore debitis esse! Qui nesciunt, assumenda sed labore
+          vitae temporibus? Quaerat magnam provident debitis tempora ullam ab
+          nobis soluta deserunt, asperiores et!
         </p>
         <div className="w-[33%] h-[360px] rounded-lg bg-nemo-dark-graph mt-4 p-6 flex flex-col justify-center items-center text-black mr-4 drop-shadow-xl">
-          <h2 className="text-white">{label1}</h2>
+          <h2 className="text-white text-xl font-semibold my-1">{label1}</h2>
           <Pie data={data1} options={options} />
         </div>
         <div className="w-[33%] h-[360px] rounded-lg bg-nemo-dark-graph mt-4 p-6 flex flex-col justify-center items-center text-black drop-shadow-xl">
-          <h2 className="text-white">{label2}</h2>
+          <h2 className="text-white text-xl font-semibold my-1">{label2}</h2>
           <Pie data={data2} options={options} />
         </div>
       </div>
--- components/person-profile/index.jsx
+++ components/person-profile/index.jsx
@@ -1,7 +1,16 @@
-import React from "react";
 import Image from "next/image";
 
 const PersonProfile = ({ data, handleUserBioRoute }) => {
+  const splitNameByWhitespace = (name) => {
+    const words = name.split(" ");
+    const half = Math.ceil(words.length / 2);
+    const firstHalf = words.slice(0, half).join(" ");
+    const secondHalf = words.slice(half).join(" ");
+    return [firstHalf, secondHalf];
+  };
+
+  const [firstLine, secondLine] = splitNameByWhitespace(data.Name);
+
   return (
     <div
       className="flex flex-col justify-center items-center cursor-pointer w-[200px] mb-4 relative"
@@ -10,22 +19,26 @@ const PersonProfile = ({ data, handleUserBioRoute }) => {
       <div className="relative">
         <Image
           src={data.profile_img}
-          width={60}
-          height={60}
+          width={90}
+          height={90}
           className="rounded-full"
           alt="profile"
         />
-        <div className="absolute bottom-0 right-[-8px]">
+        <div className="absolute bottom-[-2px] right-[-10px]">
           <Image
             src={"/images/discord.png"}
-            width={25}
-            height={25}
+            width={35}
+            height={35}
             className="rounded-full"
             alt="logo"
           />
         </div>
       </div>
-      <p className="text-gray-100 text-base text-center mt-1">{data.Name}</p>
+      <p className="text-gray-100 text-base text-center mt-1">
+        {firstLine}
+        <br />
+        {secondLine}
+      </p>
     </div>
   );
 };
